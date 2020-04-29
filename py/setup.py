e9463d69463dabf56edd56a0ec7b050725ddf982
Jason Chin <cschin@infoecho.net>
2020-04-29 17:03:34 +0000

    remove unused dependency

--- py/setup.py
+++ py/setup.py
@@ -14,9 +14,4 @@ setup(name='mater',
       cffi_modules=["mater/build_shimmer4py.py:ffibuilder"],
       install_requires=["cffi>=1.12.0",
                         "docopt>=0.6.2",
-                        "numpy>=1.16.2",
-                        "vcfpy==0.12.1",
-                        "ncls==0.0.52",
-                        "pysam==0.15.3",
-                        "networkx>=2.4",
-                        "intervaltree==3.0.2"])
+                        "numpy>=1.16.2"])
